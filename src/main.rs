b7993e62c40f99616107bb7f757b388da3fa48e2
samarth kulshrestha <samarthkulshrestha@protonmail.com>
2025-02-28 10:56:28 +0530

    implemented fast_fibonacci

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use num_bigint::{BigInt, Sign};
+
+
+fn fib_luc(mut n: isize) -> (BigInt, BigInt) {
+    if n == 0 {
+        return (BigInt::ZERO, BigInt::new(Sign::Plus, [2].to_vec()));
+    }
+
+    if n < 0 {
+        n *= -1;
+        let (fib, luc) = fib_luc(n);
+        let k = n % 2 * 2 - 1;
+        return (fib * k, luc * k)
+    }
+
+    if n & 1 == 1 {
+        let (fib, luc) = fib_luc(n - 1);
+        return (&fib + &luc >> 1, 5 * &fib + &luc >> 1)
+    }
+
+    n >>= 1;
+    let k = n % 2 * 2 - 1;
+    let (fib, luc) = fib_luc(n);
+    (&fib * &luc, &luc * &luc + 2 * k)
+}
+
+fn main() {
+    print!("enter n: ");
+    std::io::Write::flush(&mut std::io::stdout()).expect("flush failed!");
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+    s = s.trim().to_string();
+
+    let n = s.parse::<isize>().unwrap();
+    let start = std::time::Instant::now();
+
+    let fib = fib_luc(n).0;
+    let elapsed = start.elapsed();
+
+    println!("{}", fib);
+    println!("time: {:?}", elapsed);
+}
